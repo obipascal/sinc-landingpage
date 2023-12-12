c6cb50e260232e8ab2d299c329d67de307512382
Obi Pascal <pascalobi83@gmail.com>
2023-12-12 14:24:47 +0100

    updated

--- src/lib/validator/reviewValidator.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup"
-
-export const ReviewValidationSchema = yup.object().shape({
-	name: yup.string().required("Name is required"),
-	company: yup.string().required("Company is required"),
-	position: yup.string().required("Position is required"),
-	contact: yup.string().email("Conntact email is invalid."),
-	message: yup.string().required("Review Message is required")
-})
